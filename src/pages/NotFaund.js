4855b608305b9fb6c45da4e91e70583729a12a3d
fcosar <fdmcsr0133@gmail.com>
2023-03-09 13:46:13 +0300

    header

--- src/pages/NotFaund.js
+++ src/pages/NotFaund.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
  
  
  const NotFaund = () => {
    return (
-     <div>404 Not Faund</div>
+    
+     <div>
+       <Header/>
+       404 Not Faund</div>
    )
  }
  export default NotFaund
